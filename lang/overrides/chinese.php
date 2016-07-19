3b564c50dce3e6f9c3b0b22cb9be2adbe3e9d5d1
tension <tension@me.com>
2016-07-20 05:21:59 +0800

    分离语言包

---
+++ lang/overrides/chinese.php
@@ -0,0 +1,33 @@
+<?php
+	
+	
+////////// NeWorld Theme
+$_LANG['save'] = "优惠";
+$_LANG['features'] = "产品特色";
+$_LANG['pricing'] = "价格中心";
+$_LANG['themes'] = "主题界面";
+$_LANG['modules'] = "插件模块";
+$_LANG['forum'] = "讨论社区";
+$_LANG['licensing'] = "授权查询";
+$_LANG['getitnow'] = "立刻购买";
+$_LANG['ram'] = "内存";
+$_LANG['learnmore'] = "查看详情";
+$_LANG['tos'] = "服务条款";
+$_LANG['yourname'] = "您的名字";
+$_LANG['createapassword'] = "创建一个密码";
+$_LANG['getstarted'] = "开始注册";
+$_LANG['homecontact'] = "联系我们";
+$_LANG['footabout'] = "关于我们";
+$_LANG['footproduct'] = "产品中心";
+$_LANG['footsupport'] = "技术支持";
+$_LANG['footconnect'] = "社交网络";
+$_LANG['clikingiagree'] = "购买产品 表示 同意遵守 NeWorld's";
+$_LANG['copy'] = "复制";
+$_LANG['chooseproduct'] = "选择产品";
+$_LANG['summary'] = "所选产品";
+$_LANG['noaccount'] = "您还没有注册账户?";
+$_LANG['demo'] = "查看演示";
+$_LANG['sliderdesc'] = "响应式网站设计，支持各种设备访问，给您带来最良好的用户体验！完全集成 WHMCS 程序，包含 5 个自定义页面，您只需要编辑 tpl 文件即可轻松更改页面内容。全新设计用户中心让用户轻松上手、可选简化注册让购物流程更加快捷";
+$_LANG['whatis'] = "What is NeWorld";
+$_LANG['whatisdesc'] = "全站响应式设计，完全符合 W3C 标准更注重用户体验，基于 CSS3、HTML5 制作，全新的用户体验！作品覆盖各大平台让您的业务开展的更顺利！";
+$_LANG['homeannouncements'] = "公告通知：";
---
+++ lang/overrides/english.php
@@ -0,0 +1,32 @@
+<?
+
+////////// NeWorld Theme
+$_LANG['save'] = "Save";
+$_LANG['features'] = "features";
+$_LANG['pricing'] = "pricing";
+$_LANG['themes'] = "themes";
+$_LANG['modules'] = "modules";
+$_LANG['forum'] = "forum";
+$_LANG['licensing'] = "licensing";
+$_LANG['getitnow'] = "Buy Now";
+$_LANG['ram'] = "Ram";
+$_LANG['learnmore'] = "Learn More";
+$_LANG['tos'] = "Terms of Services";
+$_LANG['yourname'] = "Your name";
+$_LANG['createapassword'] = "Create a Password";
+$_LANG['getstarted'] = "GET STARTED, ITS FREE!";
+$_LANG['homecontact'] = "Contact";
+$_LANG['footabout'] = "ABOUT";
+$_LANG['footproduct'] = "PRODUCT";
+$_LANG['footsupport'] = "SUPPORT";
+$_LANG['footconnect'] = "CONNECT";
+$_LANG['clikingiagree'] = "By cliking &quot;Get it Now&quot; I agree to NeWorld's";
+$_LANG['copy'] = "Copy";
+$_LANG['chooseproduct'] = "Choose Product";
+$_LANG['summary'] = "Summary";
+$_LANG['noaccount'] = "Don't have an account yet?";
+$_LANG['demo'] = "DEMO";
+$_LANG['sliderdesc'] = "Fully responsive Website Template fully integrated in WHMCS client area for Web Hosting Providers with 5 additional pages which are included within WHMCS by using WHMCS additional pages, You just need to edit .tpl files for contents modification.";
+$_LANG['whatis'] = "What is NeWorld";
+$_LANG['whatisdesc'] = "Responsive design, accord with the W3C standards, using CSS 3, HTML 5 production based on WHMCS version 6, the user center for new production";
+$_LANG['homeannouncements'] = "announcements：";
